dc9f290329d73a5a80cb7ffd4f447c2048399bea
Genocider7 <blazerwarrior@gmail.com>
2023-01-20 13:52:40 +0100

    Ćwiczenia 11

--- 11/Zadanie2/run10times.sh
+++ 11/Zadanie2/run10times.sh
@@ -1,7 +1,8 @@
 echo starting work > output.out
 cd ..
-for i in {1..10}
+for i in {1..100}
 do
+echo test nr $i >> Zadanie2/output.out
 java Zadanie2/Main | grep "counted" >> Zadanie2/output.out
 echo -en '\n' >> Zadanie2/output.out
 done
